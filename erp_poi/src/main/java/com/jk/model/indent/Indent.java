e7ed241f7063f75e9e950398e1b2e1d65e1f6561
0xiaochuan0 <1538664852@qq.com>
2019-01-02 11:29:54 +0800

    model indent 消失重新提交

---
+++ erp_poi/src/main/java/com/jk/model/indent/Indent.java
@@ -0,0 +1,88 @@
+package com.jk.model.indent;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Indent implements Serializable {
+
+    private static final long serialVersionUID = -567867669516400134L;
+    private Integer id;
+
+    private Date creatTime;
+
+    private String consumer;
+
+    private Long money;
+
+    private Integer payId;
+
+    private Integer fromId;
+
+    private Integer payStatus;
+
+    private String goods;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreatTime() {
+        return creatTime;
+    }
+
+    public void setCreatTime(Date creatTime) {
+        this.creatTime = creatTime;
+    }
+
+    public String getConsumer() {
+        return consumer;
+    }
+
+    public void setConsumer(String consumer) {
+        this.consumer = consumer == null ? null : consumer.trim();
+    }
+
+    public Long getMoney() {
+        return money;
+    }
+
+    public void setMoney(Long money) {
+        this.money = money;
+    }
+
+    public Integer getPayId() {
+        return payId;
+    }
+
+    public void setPayId(Integer payId) {
+        this.payId = payId;
+    }
+
+    public Integer getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(Integer fromId) {
+        this.fromId = fromId;
+    }
+
+    public Integer getPayStatus() {
+        return payStatus;
+    }
+
+    public void setPayStatus(Integer payStatus) {
+        this.payStatus = payStatus;
+    }
+
+    public String getGoods() {
+        return goods;
+    }
+
+    public void setGoods(String goods) {
+        this.goods = goods == null ? null : goods.trim();
+    }
+}
